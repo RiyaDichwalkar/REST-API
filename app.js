6916c8084d7453f855280f8204ca98fe2b9efb05
RiyaDichwalkar <42038984+RiyaDichwalkar@users.noreply.github.com>
2019-08-15 10:09:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const mongoose =require('mongoose');
 const cors=require('cors');
 
 const productRoutes =require('./routes/products');
+//your atlas cluster  connection string
 const uri ='mongodb://m001-student:'+process.env.MONGO_ATLAS_PW+'@cluster1-shard-00-01-apx3g.mongodb.net:27017,cluster1-shard-00-00-apx3g.mongodb.net:27017,cluster1-shard-00-02-apx3g.mongodb.net:27017/test?ssl=true&replicaSet=Cluster1-shard-0&authSource=admin&retryWrites=true&w=majority';
 //const uri ='mongodb://localhost/FoodHousie';
 mongoose.connect(
